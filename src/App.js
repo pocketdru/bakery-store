b5088a329f051fd64b5cb21c8590c1135439a56e
Mary <pocketdru@gmail.com>
2019-04-22 22:45:00 -0600

    commit1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+
+function App() {
+    return( 
+        <div></div>
+    )
+}
+
+export default App;
